98419fba36ecff293c93c267ccf451a67011c806
Tony Inuma <tony.inuma@gmail.com>
2026-09-10 01:38:18 -0500

    add repository to list, get and create a account

---
+++ app/repositories/account_repository.py
@@ -0,0 +1,44 @@
+from app.schemas.account import (
+    AccountCreate,
+    AccountStatus,
+)
+
+
+class AccountRepository:
+    def __init__(self) -> None:
+        self._accounts: list[dict] = []
+        self._current_id = 0
+
+    async def find_all(self) -> list[dict]:
+        return self._accounts
+
+    async def find_by_id(
+        self,
+        account_id: int,
+    ) -> dict | None:
+        return next(
+            (
+                account
+                for account in self._accounts
+                if account["id"] == account_id
+            ),
+            None,
+        )
+
+    async def create(
+        self,
+        data: AccountCreate,
+    ) -> dict:
+        self._current_id += 1
+
+        account = {
+            "id": self._current_id,
+            "customer_name": data.customer_name,
+            "currency": data.currency,
+            "status": AccountStatus.ACTIVE,
+            "balance": 0.0,
+        }
+
+        self._accounts.append(account)
+
+        return account
