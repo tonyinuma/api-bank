e79ccb28ac4922559520ed427c3538747beea6ef
Tony Inuma <tony.inuma@gmail.com>
2026-09-10 01:30:34 -0500

    configure ignores and create basic structure

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="My API",
+    version="1.0.0",
+)
+
+
+@app.get("/")
+async def root():
+    return {
+        "message": "FastAPI running"
+    }
